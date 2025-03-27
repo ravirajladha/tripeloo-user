c4520a5bec225447f2db20bb8fca3510e3ba8452
ravirajladha <ravirajladha@gmail.com>
2025-03-27 20:40:20 +0530

    boking actions fix

--- src/actions/bookingActions.ts
+++ src/actions/bookingActions.ts
@@ -4,10 +4,15 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const createBooking = async (bookingData: any) => {
   // checkAuthStatus(); 
+
   // ✅ Check user authentication before making API call
 console.log(bookingData, "response being sent for booking")
   try {
+    const token = localStorage.getItem('accessToken'); 
+
+    
     const response = await axios.post(`${BACKEND_URL}/api/v1/booking`, bookingData, {
+      headers: { Authorization: `Bearer ${token}` },
       withCredentials: true, // ✅ Ensure credentials are included
     });
     return response.data;
